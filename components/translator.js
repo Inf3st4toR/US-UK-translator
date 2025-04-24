d6f44a4053e68503baa83c0efb5cfcf51c2db39d
Inf3st4toR 1 <xcharaix@gmail.com>
2025-04-24 07:11:17 +0000

    Coded logic (miss punctuation) first unit test pass

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,59 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    reverseTranslate(dico, word) {
+        for (const [key, value] of Object.entries(dico)) {
+            if (value === word) return key;
+        }
+        return false;
+    }
+
+    translateUSTime(time) {
+        const [hours, minutes] = time.split(":").map(Number);
+        if (hours > 24 || minutes > 60) return time;
+        return time.replace(":", ".");
+    }
+
+    translateUKTime(time) {
+        const [hours, minutes] = time.split(".").map(Number);
+        if (hours > 24 || minutes > 60) return time;
+        return time.replace(".", ":");
+    }
+
+    us2uk(word) {
+        return (americanOnly[word] || americanToBritishSpelling[word] || americanToBritishTitles[word] || word);
+    }
+
+    uk2us(word) {
+        return (britishOnly[word] || reverseTranslate(americanToBritishSpelling, word) || reverseTranslate(americanToBritishTitles, word) || word);
+    }
+
+    // consider punctuation
+    textProcess(text, locale) {
+        const textSplit = text.split(" ");
+        const regexUS = /^\d{2}:\d{2}$/;
+        const regexUK = /^\d{2}.\d{2}$/;
+
+        //US to UK translation
+        if (locale === "american-to-british") {
+            const array0 = textSplit.map(word => {
+                if (regexUS.test(word)) return this.translateUSTime(word);
+                return this.us2uk(word);
+            });
+            return array0.join(" ");
+        }
+
+        //UK to US translation
+        if (locale === "british-to-american") {
+            const array1 = textSplit.map(word => {
+                if (regexUK.test(word)) return this.translateUKTime(word);
+                return this.uk2us(word);
+            });
+            return array0.join(" ");
+        }
+
+        else return { error: 'Invalid value for locale field' };
+    }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,13 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator;
 
 suite('Unit Tests', () => {
-
+    test('Translation 1', () => {
+        const text = "Mangoes are my favorite fruit.";
+        const translation = translator.textProcess(text, "american-to-british");
+        console.log(translation);
+        assert.equal(translation, "Mangoes are my favourite fruit.", 'Error in translation');
+      });
 });
